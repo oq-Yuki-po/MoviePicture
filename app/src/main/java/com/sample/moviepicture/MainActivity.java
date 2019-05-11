747a8064c163622015197a82be1169c5997570ed
oq-Yuki-po <o.yukinko0715@gmail.com>
2019-05-11 14:41:15 +0900

    modify main activity and layout to attach Fragment

--- app/src/main/java/com/sample/moviepicture/MainActivity.java
+++ app/src/main/java/com/sample/moviepicture/MainActivity.java
@@ -3,11 +3,16 @@ package com.sample.moviepicture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.ar.sceneform.ux.ArFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     }
 }
